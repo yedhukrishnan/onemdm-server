bba3bd77446401f167fb06c9932df30e2a1400e6
leenasn <leena.sn@multunus.com>
2014-11-07 17:49:17 +0530

    Add the Production DB configuration to connection.js

--- config/connections.js
+++ config/connections.js
@@ -80,7 +80,7 @@ module.exports.connections = {
     user: 'YOUR_POSTGRES_USER',
     password: 'YOUR_POSTGRES_PASSWORD',
     database: 'YOUR_POSTGRES_DB'
-  }
+  },
 
 
   /***************************************************************************
@@ -88,5 +88,9 @@ module.exports.connections = {
   * More adapters: https://github.com/balderdashy/sails                      *
   *                                                                          *
   ***************************************************************************/
-
+  postgresProductionDB: {
+    adapter: 'sails-postgresql',
+    url: process.env.HEROKU_POSTGRESQL_COBALT_URL
+  } 
+ 
 };
