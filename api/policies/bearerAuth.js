0e0dbe2aaa4525092b5a7317b07a082d6ade4efd
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2014-12-05 11:07:30 +0530

    Refactor bearerAuth

--- api/policies/bearerAuth.js
+++ api/policies/bearerAuth.js
@@ -1,8 +1,6 @@
 module.exports = function(req, res, next) {
 
-  if(req.isSocket) {
-    next();
-  }
-  return passport.authenticate('bearer', { session: false })(req, res, next);
+  if(req.isSocket) next();
+  else return passport.authenticate('bearer', { session: false })(req, res, next);
   
 };
