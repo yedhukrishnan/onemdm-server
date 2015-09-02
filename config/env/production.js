f720adb350602b8449fa76578044a41e4a7abb34
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2015-09-02 10:23:28 +0530

    Add migrate: safe to production heroku account
     - This is not a suggested approach, but we need this to migrate
       databases in Heroku until we find some other way :)

--- config/env/production.js
+++ config/env/production.js
@@ -29,7 +29,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80
+  port: 80,
+  migrate: 'safe'
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
